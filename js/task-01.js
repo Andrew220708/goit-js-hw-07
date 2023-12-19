54757a250907691cee3fcaed9957e683011f09a3
Andrew220708 <andrew.asmogilov@gmail.com>
2023-12-19 16:08:42 +0100

    Update taska 1
    commit

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+// console.dir(document);
+const categoriesEl = document.querySelector('#categories');
+console.dir(categoriesEl);
+const itemEl = document.querySelectorAll('.item');
+console.log(itemEl);
