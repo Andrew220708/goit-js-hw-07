62d9a9e6780f63385576176c3a218b28b09d99ed
Andrew220708 <andrew.asmogilov@gmail.com>
2024-01-09 16:51:06 +0100

    modification of dz-7
    modification of dz-7

--- js/task-2.js
+++ js/task-2.js
@@ -27,82 +27,21 @@ const images = [
     alt: 'Zebras on Zebra',
   },
 ];
-// *************1. Рішення.****************
-// const firstEl = document.createElement('li');
-// const catEl = document.createElement('img');
-// catEl.src = 
-//   'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-// catEl.alt = 'White and Black Long Fur Cat';
-// catEl.width = 360;
-// catEl.height = 300;
+const createEl = ({ url, alt }) =>
+  `<li  class="li">
+       <img class="img"
+       src="${url}"
+        alt="${alt}" />
+  </li>`;
 
-// const secondEl = document.createElement('li');
-// // secondEl.classList.add(gallery_item); Підкажіть чьому я не можу добавити клас?
-// const fishEl = document.createElement('img');
-// fishEl.src =
-//   'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-// fishEl.alt = 'Orange and White Koi Fish Near Yellow Koi Fish';
-// fishEl.width = 360;
-// fishEl.height = 300;
+const elem = images.map(createEl).join('');
 
-// const thirdEl = document.createElement('li');
-// // thirdEl.classList.add(gallery_item);
-// const horsesEl = document.createElement('img');
-// horsesEl.src =
-//   'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260';
-// horsesEl.alt = 'Group of Horses Running';
-// horsesEl.width = 360;
-// horsesEl.height = 300;
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', elem);
+gallery.style.display = 'flex';
+gallery.style.gap = '24px';
 
-// const fourthEl = document.createElement('li');
-// // fourthEl.classList.add(gallery_item);
-// const elephantEl = document.createElement('img');
-// elephantEl.src =
-//   'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
-// elephantEl.alt = 'Elephant Beside on Baby Elephant';
-// elephantEl.width = 360;
-// elephantEl.height = 300;
-
-// const fifthEl = document.createElement('li');
-// // fifthEl.classList.add(gallery_item);
-// const parrotEl = document.createElement('img');
-// parrotEl.src =
-//   'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
-// parrotEl.alt = 'Blue Geeen and Orange Parrot';  
-// parrotEl.width = 360;
-// parrotEl.height = 300;
-
-// const sixthEl = document.createElement('li');
-// // sixthEl.classList.add(gallery_item);
-// const zebraEl = document.createElement('img');
-// zebraEl.src =
-//   'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
-// zebraEl.alt = 'Zebras on Zebra';
-// zebraEl.width = 360;
-// zebraEl.height = 300;
-// const galleryEl = document.querySelector('.gallery');
-// firstEl.append(catEl);
-// secondEl.append(fishEl);
-// thirdEl.append(horsesEl);
-// fourthEl.append(elephantEl);
-// fifthEl.append(parrotEl);
-// sixthEl.append(zebraEl);
-
-// galleryEl.append(firstEl, secondEl, thirdEl, fourthEl, fifthEl, sixthEl);
-
-// *************2. Рішення.******************
-const galleryEl = document.querySelector('.gallery');
-
-
-const createMarkup = ({ url, alt }) =>
-  `
-  <li>
-   <img class="image" src=${url} alt=${alt} width='360' height='300'
-  </li>
- 
-  `; 
-
-const markup = images.map(createMarkup).join('');
-  
-galleryEl.insertAdjacentHTML('beforeend', markup);
-
+const imagesElements = gallery.querySelectorAll('img');
+imagesElements.forEach(img => {
+  img.style.width = '100%';
+});
--- js/task-5.js
+++ js/task-5.js
@@ -3,8 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const clickMe = document.querySelector('.change-color');
-const colorBox = document.querySelector('.color');
-clickMe.addEventListener('click', (event) => {
-colorBox.style.backgroundColor = getRandomHexColor();
+const buttonChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', event => {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  spanColor.textContent = color;
 });
